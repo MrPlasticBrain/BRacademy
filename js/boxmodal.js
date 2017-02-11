e67edc4b4380c216d5375b318454c7d2cb8f77ae
mrplasticbrain <alekmilanov.sr@gmail.com>
2017-02-12 00:19:32 +0100

    add questions

--- js/boxmodal.js
+++ js/boxmodal.js
@@ -11,20 +11,40 @@ var span = document.getElementsByClassName("close")[0];
 /*btn.onclick = function() {
     modal.style.display = "block";
 } */
+
 $(document).on('click', '.grid_3 a', function (e) {
-    console.log('bravoo');
-    console.log($(e.currentTarget));
     $('#myModal').show(); 
+    fillOptions();
 });
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+$(document).on('click','.close', function(element){
+    $('#myModal').hide();
+});
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
+    }
+}
+
+function fillOptions(){
+    var myDiv = document.getElementById("years");
+
+    //Create array of options to be added
+    var array = ["1","2","3","4","5"];
+
+    //Create and append select list
+    var selectList = document.createElement("select");
+    selectList.id = "mySelect";
+    myDiv.appendChild(selectList);
+
+    //Create and append the options
+    for (var i = 0; i < array.length; i++) {
+        var option = document.createElement("option");
+        option.value = array[i];
+        option.text = array[i];
+        selectList.appendChild(option);
     }
 }
